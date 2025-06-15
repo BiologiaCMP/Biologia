d93052ef0a6bac4376475c02b86fc236829bee1f
BiologiaCMP <biologiacmp39@gmail.com>
2025-06-14 21:06:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function showTab(tabName) {
+  document.querySelectorAll('.tab-content').forEach(tab => tab.style.display = 'none');
+  document.getElementById(tabName).style.display = 'block';
+}
+
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('show');
+}
